e79ee1edde87143ce4f02452ba8ee3b466d915aa
Eka Fajhari Adwar <eka.adwar@gmail.com>
2021-10-11 22:30:54 +0700

    Instalasi redux, react-redux, redux-thunk, redux-logger; membuat reducer cart

--- App.js
+++ App.js
@@ -6,17 +6,21 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import ProductDetail from "./src/screens/ProductDetail";
+import { Provider } from "react-redux";
+import store from "./src/redux/store";
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Dashboard" }} />
-        <Stack.Screen name="Detail" component={ProductDetail} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Dashboard" }} />
+          <Stack.Screen name="Detail" component={ProductDetail} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
